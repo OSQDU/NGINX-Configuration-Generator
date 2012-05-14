986320a90b4fd771e1bb334ff1f26939b81d730f
Yinchuan Song <songyinchuan@gmail.com>
2012-05-13 21:24:24 -0400

    prepare for template processing

--- ngx_confgen.pl
+++ ngx_confgen.pl
@@ -2,12 +2,14 @@
 use strict;
 use Data::Dumper;
 use Getopt::Long;
+use File::Spec;
+use File::Basename;
 
 my @static_apps;
 my @php_apps;
-my @proxy_servers;
 my @rack_apps;
 my @python_apps;
+my @proxy_servers;
 
 GetOptions (
     "static=s" => \@static_apps,
@@ -16,16 +18,56 @@ GetOptions (
     "rack=s" => \@rack_apps,
     "python=s" => \@python_apps);
 
-my @static;
-foreach my $static_app (@static_apps) {
-    my @fields = split /:/,$static_app;
-    my $server_names = join(" ",split (/,/, shift @fields));
-    my $server_root = shift @fields;
-    my %static_item = ('server_name' => $server_names, 'server_root' => $server_root);
-    push @static, \%static_item;
+my $static = hosted_app_info(\@static_apps,"static");
+my $php = hosted_app_info(\@php_apps, "php");
+my $rack = hosted_app_info(\@rack_apps, "rack");
+my $python = hosted_app_info(\@python_apps, "python");
+my $proxy = proxy_info(\@proxy_servers, "reverseproxy");
+#print Dumper($static,$php,$rack,$python,$proxy);
+
+my $base_dir = dirname(File::Spec->rel2abs(__FILE__));
+my $template_dir = "$base_dir/lib/templates";
+
+my $rack_template = "$template_dir/rack.conf.tmpl";
+my $proxy_template = "$template_dir/proxy.conf.tmpl";
+
+
+print $$static[0]{'server_name'};
+
+sub hosted_app_info {
+    my $apps = shift; # Get a reference of applications list
+    my $app_type = shift;
+    my @apps = @$apps; # Dereference application list
+    my @servers; # Defined servers
+    foreach my $app (@apps) {
+	my @fields = split /:/, $app;
+	my $server_name = join(" ", split(/,/, shift @fields));
+	my $server_root = shift @fields;
+	my %server_instance = ('server_name' => $server_name, 'server_root' => $server_root, 'type' => $app_type);
+	push @servers, \%server_instance;
+    }
+    return \@servers;
 }
 
-print Dumper(\@static);
-print Dumper(\@static_apps,\@php_apps,\@proxy_servers,\@rack_apps,\@python_apps);
+sub proxy_info {
+    my $proxies = shift;
+    my $proxy_type = shift;
+    my @proxies = @$proxies;
+    my @servers;
+    foreach my $proxy (@proxies) {
+	my @fields = split /:/, $proxy, 2; # Only split to 2 parts;
+	my $server_name = join(" ", split(/,/, shift @fields));
+	my @backends = split(/,/, shift @fields);
+	my %proxy_instance = ('server_name' => $server_name, 'backend' => \@backends, 'type' => $proxy_type);
+	push @servers, \%proxy_instance;
+    }
+    return \@servers;
+}
 
-sub error_format {}
+sub process_template {
+    my $template = shift;
+    my $arguments = shift;
+    my $output;
+
+    return $output;
+}
